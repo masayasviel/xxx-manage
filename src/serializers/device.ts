459797be1c387ca6dcf4ccde54d12098460bd88b
masayasviel <33654883+masayasviel@users.noreply.github.com>
2023-04-22 01:35:57 +0900

    :sparkles: register

---
+++ src/serializers/device.ts
@@ -0,0 +1,21 @@
+import { z } from 'zod';
+
+const CODE_REGEX = /^[0-9]{1,2}$/;
+
+export const DeviceRegisterRequestSchema = z.object({
+    maker: z.string().optional(),
+    category: z.string().regex(CODE_REGEX),
+    name: z.string(),
+    tepla: z.string(),
+    os: z.string().optional(),
+    osVersion: z.string().optional(),
+    isEmergencyContact: z.boolean(),
+    isCapitalization: z.boolean(),
+    amount: z.number().min(0),
+    purchaseDate: z.string().datetime({ offset: true }).optional(),
+    storageLocate: z.string().optional(),
+    deviceStatus: z.string().regex(CODE_REGEX),
+    remarks: z.string().optional(),
+});
+
+export type DeviceRegisterRequestType = z.infer<typeof DeviceRegisterRequestSchema>;
---
+++ src/services/device-register.ts
@@ -0,0 +1,25 @@
+import { DbClient } from '../core/db-client';
+import {
+    DeviceRegisterRequestType
+} from '../serializers/device';
+
+export const register = async (data: DeviceRegisterRequestType): Promise<void> => {
+    const client = DbClient.instance;
+    await client.device.create({
+        data: {
+            maker: data.maker ?? null,
+            category: data.category,
+            name: data.name,
+            tepla: data.tepla,
+            os: data.os ?? null,
+            osVersion: data.osVersion ?? null,
+            isEmergencyContact: data.isEmergencyContact,
+            isCapitalization: data.isCapitalization,
+            amount: data.amount,
+            purchaseDate: data.purchaseDate ?? null,
+            storageLocate: data.storageLocate ?? null,
+            deviceStatus: data.deviceStatus,
+            remarks: data.remarks ?? null,
+        }
+    })
+}
